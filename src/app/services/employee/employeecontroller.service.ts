d7b5da790b9670dce7eef17e000fc62bf3e40f3d
KidRomesh <romikhanna1301@gmail.com>
2022-04-07 12:35:42 +0530

    merge conflict issue

--- src/app/services/employee/employeecontroller.service.ts
+++ src/app/services/employee/employeecontroller.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap, catchError, map, of } from 'rxjs';
+import { Employee } from 'src/app/models/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeecontrollerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private empUrl = 'localhost:6060/employee';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+    private log(message: string) {
+      console.log(message);
+    }
+    
+    addHero(emp: Employee): Observable<Employee> {
+      const url = `${this.empUrl}/login`;
+      return this.http.post<Employee>(url, emp, this.httpOptions)
+    }
+  
+
 }
